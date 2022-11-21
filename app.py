e2e91882b6c2c2b613363485a9fcf16a97d26f5e
neginmaka <115941409+neginmaka@users.noreply.github.com>
2022-11-20 21:03:39 -0500

    PF-5 Design and create database

--- app.py
+++ app.py
@@ -1,13 +1,65 @@
-from flask import Flask, render_template, redirect, url_for, flash, abort
-from werkzeug.security import generate_password_hash, check_password_hash
-from forms import RegisterForm, LoginForm
-from flask_bootstrap import Bootstrap
-
 import secrets
+
+from flask import Flask, render_template, redirect, url_for, json
+from flask_bootstrap import Bootstrap
+from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegisterForm, LoginForm
+from sqlalchemy.orm import relationship
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secrets.token_hex(16)
 Bootstrap(app)
+
+# CONNECT TO DB
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///dbc.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# create and initialize the app with the extension
+db = SQLAlchemy(app)
+db.init_app(app)
+
+
+# CONFIGURE TABLES
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    username = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(100))
+    profile_pic_url = db.Column(db.String(250), nullable=False)
+    bio = db.Column(db.Text, nullable=False)
+    links = relationship("Link", back_populates="user")
+
+
+class Link(db.Model):
+    __tablename__ = "links"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    user = relationship("User", back_populates="links")
+    label = db.Column(db.String(250), nullable=False)
+    url = db.Column(db.String(250), nullable=False)
+
+
+def populate_test_data():
+    """
+    If no users/links are in the database, this function populates some default users for testing purposes.
+    """
+    users = User.query.all()
+    if len(users) == 0:
+        with open("tests/test_data.json") as test_data_json:
+            test_data = json.load(test_data_json)
+            users = test_data["users"]
+            links = test_data["links"]
+            db.session.bulk_insert_mappings(User, users)
+            db.session.bulk_insert_mappings(Link, links)
+            db.session.commit()
+
+
+with app.app_context():
+    db.create_all()
+    populate_test_data()
 
 
 @app.route("/")
