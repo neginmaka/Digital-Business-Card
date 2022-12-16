cdd526950b9564338751791211f5ab6107749dd7
Negin Maka <115941409+neginmaka@users.noreply.github.com>
2022-12-17 00:43:42 +0330

    PF-28 Create func test for https routes

--- tests/functest/test_routes.py
+++ tests/functest/test_routes.py
@@ -1,21 +1,15 @@
+import pytest
+
 from main_app import app
 
 
-def test_home_page():
-    """
-    GIVEN a Flask application configured for testing
-    WHEN the '/' page is requested (GET)
-    THEN check that the response is valid
-    """
-    response = app.test_client().get('/')
-    assert response.status_code == 200
-    assert b"Welcome to the" not in response.data
+def get_application_endpoints():
+    endpoints = ["/", "/register", "/login", "/about", "/contact",
+                 "/<username>/admin", "/<username>/profile", "/<username>"]
+    return endpoints
 
-    # Create a test client using the Flask application configured for testing
-    # with flask_app.test_client() as test_client:
-    #     response = test_client.get('/')
-    #     assert response.status_code == 200
-    #     assert b"Welcome to the" in response.data
-    #     assert b"Flask User Management Example!" in response.data
-    #     assert b"Need an account?" in response.data
-    #     assert b"Existing user?" in response.data
+
+@pytest.mark.parametrize('endpoint', get_application_endpoints())
+def test_get_requests(endpoint):
+    response = app.test_client().get(endpoint)
+    assert response.status_code == 200
