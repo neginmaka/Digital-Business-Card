9e7906837f2f826335092cfecbafcc66f6535354
neginmaka <115941409+neginmaka@users.noreply.github.com>
2022-12-19 18:49:14 -0500

    PF-24 Adding more func tests

--- tests/functest/test_routes.py
+++ tests/functest/test_routes.py
@@ -3,13 +3,41 @@ import pytest
 from main_app import app
 
 
-def get_application_endpoints():
-    endpoints = ["/", "/register", "/login", "/about", "/contact",
-                 "/<username>/admin", "/<username>/profile", "/<username>"]
+def get_endpoints():
+    endpoints = {"/",
+                 "/register",
+                 "/login",
+                 "/about",
+                 "/contact",
+                 "/testuser"}
     return endpoints
 
 
-@pytest.mark.parametrize('endpoint', get_application_endpoints())
-def test_get_requests(endpoint):
-    response = app.test_client().get(endpoint)
-    assert response.status_code == 200
+def redirect_to_login_endpoints():
+    endpoints = {"/testuser/admin",
+                 "/testuser/profile"}
+    return endpoints
+
+
+def redirect_to_home_endpoints():
+    endpoints = {"/logout"}
+    return endpoints
+
+
+class TestRoutes:
+    @pytest.mark.parametrize('endpoint', get_endpoints())
+    def test_get_requests(self, endpoint):
+        response = app.test_client().get(endpoint)
+        assert response.status_code == 200
+
+    @pytest.mark.parametrize('endpoint', redirect_to_login_endpoints())
+    def test_redirect_to_login_work_correctly(self, endpoint):
+        response = app.test_client().get(endpoint)
+        assert "/login?next" in response.location
+        assert response.status_code == 302
+
+    @pytest.mark.parametrize('endpoint', redirect_to_home_endpoints())
+    def test_redirect_to_home_work_correctly(self, endpoint):
+        response = app.test_client().get(endpoint)
+        assert response.location == "/"
+        assert response.status_code == 302
